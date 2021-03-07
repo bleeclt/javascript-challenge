907d3c7da5ae14b8972ea4f3433b169d065399a4
Brandon Lee <70173407+bleeclt@users.noreply.github.com>
2021-03-07 17:04:14 -0500

    next

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -4,15 +4,18 @@ var tableData = data;
 // append table to web page and add new rows for UFO sighting
 var tbody = d3.select("tbody");
 // console.log(tableData);
+loadTable(tableData);
 
-tableData.forEach((ufo) => {
-    var row = tbody.append("tr");
-    Object.defineProperties(ufo).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+function loadTable(tableData) {
+    tableData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = newrow.append("td");
+            cell.text(value);
+        });
+
     });
-
-});
+}
 
 // js code that will listen for events and search d/t column to find rows that match user input
 var button = d3.select("#filter-btn");
@@ -26,6 +29,9 @@ function newDate() {
     var inputBox = d3.select("#datetime");
     var inputDate = inputBox.property("value");
     var filterData = tableData.filter(tableData => tableData.datetime === inputDate);
+
+    console.log(filterData);
+
     tbody.html("");
     if (filterData.length === 0) {
         tmsg = tbody.text("No data for this date");
